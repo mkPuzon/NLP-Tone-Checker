37e0d57b97f2e273f5d81f56877cbd208b98f42c
Maddie Puzon <118487929+mkPuzon@users.noreply.github.com>
2023-04-24 10:08:17 -0400

    adds main file with basic funtion to read articles

---
+++ main.py
@@ -0,0 +1,26 @@
+"""
+This file contains the tone checker using python libraries.
+"""
+from textblob import TextBlob
+from newspaper import Article
+import sys
+
+input = sys.argv
+# usage statement
+if len(input) != 2:
+    print("Needs url of an article to run.")
+    exit()
+
+url = input[1]
+article = Article(url)
+
+article.download()  # get article into script
+article.parse()     # removes html / makes readable
+article.nlp()       # preps it for nlp
+
+text = article.text  # gives all text in article (string)
+summary = article.summary   # summarizes article contents (string)
+
+blob = TextBlob(summary)
+sentiment = blob.sentiment.polarity # -1 to 1
+print(sentiment)
